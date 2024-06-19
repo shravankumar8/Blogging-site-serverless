984ba844c435239f2f0ab0b6d4093277e8641c74
shravan kumar <kumashravan5@gmail.com>
2024-06-19 12:29:35 +0530

    .

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,29 @@
 import { Hono } from 'hono'
 
 const app = new Hono()
-
+// here c in callback everything req,res,etc
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+app.post('/api/v1/user/signup', (c) => {
+  return c.text('user signup')
+})
+app.post("/api/v1/user/signin", (c) => {
+  return c.text("user sigin");
+});
+app.post("/api/v1/blog", (c) => {
+  return c.text("create blog");
+});
+app.put('/api/v1/blog', (c) => {
+  return c.text('write update a blog!')
+})
+app.get('/api/v1/blog/:id', (c) => {
+  const id=c.req.param("id")
+  console.log(id)
+  return c.text('get a specific blog')
+})
+app.get('/api/v1/blog/bulk', (c) => {
+  return c.text('blog bulk route')
 })
 
 export default app
